e1561ab53b7df9b74bcba656a6985529e5ba7f07
Ivan <mefistofeler@gmail.com>
2026-02-16 20:00:00 +0300

    fixed mapping

--- RatingService/src/main/java/com/efcon/ratingservice/rating/mapper/RatingMapper.java
+++ RatingService/src/main/java/com/efcon/ratingservice/rating/mapper/RatingMapper.java
@@ -3,13 +3,19 @@ package com.efcon.ratingservice.rating.mapper;
 import com.efcon.ratingservice.rating.dto.RatingRequestDto;
 import com.efcon.ratingservice.rating.dto.RatingResponseDto;
 import com.efcon.ratingservice.rating.model.Rating;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
 @Mapper(componentModel = "spring")
 public interface RatingMapper {
 
+
+
     Rating toEntity(RatingRequestDto dto);
+
+
     RatingResponseDto toDto(Rating rating);
 
 }
