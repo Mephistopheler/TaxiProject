e3742586bebf2f925ff88de520389ec64aca623c
Ivan <mefistofeler@gmail.com>
2026-03-24 02:23:33 +0300

    Map service unavailable exceptions to 503

--- TripService/src/main/java/com/efcon/tripservice/service/exception/ServiceUnavailableException.java
+++ TripService/src/main/java/com/efcon/tripservice/service/exception/ServiceUnavailableException.java
@@ -1,5 +1,9 @@
 package com.efcon.tripservice.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 public class ServiceUnavailableException extends RuntimeException {
 
     public ServiceUnavailableException(String message) {
